dbc500fa9d87eb6260b4c8731014814d67be45ac
lygithub <leiyu.cpp@gmail.com>
2014-09-10 17:31:42 +0800

    push1

--- com/malloc/tree/NodeTree.java
+++ com/malloc/tree/NodeTree.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import com.malloc.tree.Node.NodeType;
+
 import net.ontopia.topicmaps.core.TopicIF;
 import net.ontopia.topicmaps.core.TopicMapBuilderIF;
 import net.ontopia.topicmaps.core.TopicMapIF;
@@ -13,29 +15,38 @@ import net.ontopia.topicmaps.impl.basic.InMemoryTopicMapStore;
 
 public class NodeTree {
 
+    // 参数   当前节点（即父节点），待加入的节点的参数（index;name;topic;nodetype;）
+    // 返回   是否添加成功，父节点的getLeafList长度是否加一
+    // 流程   根据pNode.getIndex()找到父节点，添加pNode.getLeafList().add(addNode);
+    //      添加到树，然后根据pNode与addNode修改TM
+    //          
     public void AddNode(Node pNode, Node addNode) {
-        FindNode(pNode);
+        FindNode(pNode.getIndex());
         if (pNode.getLeafList() == null) {
             // pNode.getLeafList() = new ArrayList<Node>();
         }
         pNode.getLeafList().add(addNode);
 
-        // modify tm, save to xtm
+        // TODO  modify tm, save to xtm
     }
 
+    // 参数   当前节点（即子节点）
+    // 返回   是否删除成功，父节点的getLeafList是否为空
+    // 流程   遍历以delNode为根的子树，依次删除
+    //          
     public void DeleteNode(Node delNode) {
-        FindNode(delNode);
+        FindNode(delNode.getIndex());
 
-        // 深度优先遍历以delNode为根的子树
+        // TODO  深度优先遍历以delNode为根的子树
 
-        // modify tm, save to xtm
+        // TODO  modify tm, save to xtm
     }
 
-    public int FindNode(Node fNode) {
+    public Node FindNode(int fIndex) {
 
-        // 深度优先遍历树
+        // TODO  深度优先遍历树
 
-        return 1;
+        return new Node();// TODO 返回查找到的节点
     }
 
     // public boolean isLeaf() {
@@ -52,22 +63,23 @@ public class NodeTree {
 
     // for test
     public static void main(String[] args) {
-        
 
+        // TODO 读取已存在的XTM文件，在此XTM中进行添加删除
         TopicMapStoreIF store = new InMemoryTopicMapStore();
         TopicMapIF topicmap = store.getTopicMap();
         TopicMapBuilderIF builder = topicmap.getBuilder();
-        
 
         String name = "qqq";
         TopicIF t1 = builder.makeTopic();
         builder.makeTopicName(t1, name);
-        
+
         Node s = new Scene(1, name, t1);
         System.out.println(s.getIndex());
         System.out.println(s.getName());
         System.out.println(s.getNodeType());
         System.out.println(s.getTopic().getTopicNames());
-        
+        System.out.println(s.getTopic().getObjectId());
+        System.out.println(s.getTopic().getItemIdentifiers());
+
     }
 }
